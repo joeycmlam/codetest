995a46d62d0ace9d0a46bac7f925bbc5e03f6609
joeylam <joey.cm.lam@gmail.com>
2022-09-02 14:59:36 +0800

    new function - twoSum

--- src/Solution.js
+++ src/Solution.js
@@ -16,6 +16,36 @@ export class Solution {
         return result;
     };
 
+    twoSum = function (nums, targets) {
+        let result = [];
+        let a = 0;
+        let b = 0;
+        let i = 0;
+        let j = 0;
+        let  found = false;
+
+        for (i = 0; i < nums.length; i ++) {
+            a = nums[i];
+            for (j = i+1; j < nums.length - 1; j++) {
+                b = nums[j];
+
+                if ((a + b) == targets) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                break;
+            }
+        }
+
+        if (found) {
+            result.push(i);
+            result.push(j);
+        }
+
+        return result;
+    }
 
 }
 
--- src/main.js
+++ src/main.js
@@ -7,8 +7,13 @@ const logger = log4js.getLogger();
 function main() {
     logger.level = "debug";
     const sln = new Solution();
-    let result = sln.multiply(1, 9)
-    console.log(`result = ${result}`)
+    // let result = sln.multiply(1, 9)
+
+    let result= [];
+    let input = [1, 2, 3, 4, 5];
+    let target = 9;
+    result = sln.twoSum(input, target)
+    logger.info(`intput = ${input} target = ${target}`);
     logger.info(`result = ${result}`)
 }
 

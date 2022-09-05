c11f03a460936086f5d117ead22f0398b2db2d55
joeylam <joey.cm.lam@gmail.com>
2022-09-05 23:21:43 +0800

    fix the cucumber feature steps

--- src/calculator.js
+++ src/calculator.js
@@ -1,12 +1,13 @@
 class Calculator {
 
+
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 
     addNumber () {
-        this.result = this.x + this.y;
+        this.result = (this.x + this.y);
     };
 
     getResult() {

b86cd59a14284a13a0ac22c890914baa92a89974
joeylam <joey.cm.lam@gmail.com>
2022-09-25 00:10:52 +0800

    testing

--- features/step_definitions/tree_steps.js
+++ features/step_definitions/tree_steps.js
@@ -1,13 +1,15 @@
-var {defineSupportCode} = require('cucumber');
+const assert = require('assert');
+const {Given, When, Then} = require('@cucumber/cucumber');
 
-defineSupportCode(function ({And, But, Given, Then, When}) {
-    Given(/^provide  "([^"]*)"$/, function (arg1, callback) {
-        callback.pending();
-    });
-    When(/^run the program$/, function (callback) {
-        callback.pending();
-    });
-    Then(/^out is "([^"]*)"$/, function (arg1, callback) {
-        callback.pending();
-    });
+Given("provide  {str}", function (arg1) {
+    callback.pending();
+});
+
+When(/^run the program$/, function (callback) {
+    // do nothing
+});
+
+Then("the result is {boolan}", function (expectedResult) {
+    actualResult = false;
+    assert.equal(actualResult, expectedResult);
 });
